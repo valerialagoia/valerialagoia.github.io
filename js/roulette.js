0f0884be01d20209693cef4bd582ee54df3fc1b0
Manuel <spacewebdesignitalia@gmail.com>
2018-07-15 23:07:12 +0200

    Added JS file

---
+++ js/roulette.js
@@ -0,0 +1,9 @@
+var cheat_meals = [
+    { 'Name': 'Pizza' },
+    { 'Name': 'Sushi' },
+    { 'Name': 'Montaditos' },
+    { 'Name': 'Flower Burger' },
+    { 'Name': 'Chorillano' },
+    { 'Name': 'Messicano' }
+];
+
