f73f4ca67398c5d2b30ef0e84cb5b10a884fa4d3
Manuel <spacewebdesignitalia@gmail.com>
2018-07-25 15:31:33 +0200

    Roulette script added

--- js/roulette.js
+++ js/roulette.js
@@ -7,3 +7,23 @@ var cheat_meals = [
     { 'Name': 'Messicano' }
 ];
 
+var $roulette = $('.roulette');
+
+$('.spin').on('click', function() {
+    var total = cheat_meals.length,
+        selected = Math.floor( Math.random() * total*5 ),
+        i = 0;
+
+
+        for ( i = 0; i <= total*5; i++) {
+            setTimeout((function(i){
+                return function(){
+                    $roulette.text( cheat_meals[i%total].Name.toUpperCase() );
+                };
+            }(i)), i * 100 );
+
+            if ( i === selected ) {
+                break;
+            }
+        }
+})
